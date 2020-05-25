4de2e628a0848c40c8b9db0272b81fbf39f05f03
Neuton Foo <neuton.foo@gmail.com>
2020-05-25 11:04:20 +0800

    The search update

--- js/makan.js
+++ js/makan.js
@@ -1,7 +1,9 @@
-function loadPlaces(fileName, $place_container) {
+function loadPlaces(fileName, $place_container, accent) {
 	$.getJSON(fileName, function (makan_guide_json) {
 		for (place of makan_guide_json) {
-			const place_html = `<div class="col-6 col-md-4 col-lg-3 d-flex align-items-stretch">
+			const place_html = `<div class="col-6 col-md-4 col-lg-3 d-flex align-items-stretch place" data-tags="${place.name.toLowerCase()} ${place.areas
+				.join(' ')
+				.toLowerCase()} ${place.tags.join(' ').toLowerCase()}">
             <div class="card place_card my-2">
                 <a href="${place.yelp}" target="_blank">
                     <img src="${place.thumbnail}" class="card-img-top" alt="${
@@ -10,7 +12,10 @@ function loadPlaces(fileName, $place_container) {
                 </a>
                 <div class="card-body">
                     <h5 class="card-title">${place.name}</h5>
-                    <p class="card-text">${place.tags.join(', ')}</p>
+                    <h6 class="card-title areas ${accent}">${place.areas.join(
+				', '
+			)}</h6>
+                    <p class="card-text tags">${place.tags.join(', ')}</p>
                 </div>
                 <div class="card-footer bg-transparent">
                     <a href="${
@@ -25,3 +30,15 @@ function loadPlaces(fileName, $place_container) {
 		}
 	});
 }
+
+$('#makan-search').on('keyup', function () {
+	const query = $(this).val().toLowerCase();
+
+	$('.place').each(function () {
+		if ($(this).data('tags').includes(query)) {
+			$(this).show();
+		} else {
+			$(this).attr('style', 'display:none !important');
+		}
+	});
+});
